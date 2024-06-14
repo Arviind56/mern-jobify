fe59382378bbbd8ce1acc50208990f20e0b9ee7d
Arvind Rajagopalan <arvindrajagopalan87@gmail.com>
2024-06-14 15:53:11 +0530

    update

--- client/src/utils/customFetch.js
+++ client/src/utils/customFetch.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const customFetch = axios.create({
-  baseURL: "https://mern-jobify-7ani.onrender.com/",
+  baseURL: "https://mern-jobify-7ani.onrender.com/api/v1",
 });
 
 export default customFetch;
